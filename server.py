7d2d4c66f2c93476fff51268fd3df2df7db98e18
Dikshant Naik <dikshantlnaikxyz@gmail.com>
2024-01-21 16:38:31 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-from multiprocessing import Pool
-from multiprocessing import cpu_count
-import time
+from flask import Flask, request
+import subprocess
+import socket
 
-# Do intensive computation to stress the CPU
-def stress_cpu(n):
-    total = 0
-    for i in range(n):
-        total += i**2
-    return total
+app = Flask(__name__)
 
-start_time = time.time()
-# Create as many as processes as there are CPU cores
-processes = cpu_count()
-pool = Pool(processes)
-print(pool.map(stress_cpu, [110000000, 110000000]))
-print("time cost: ", time.time() - start_time)
+@app.route("/", methods=["POST"])
+def stress_cpu():
+    subprocess.Popen(["python3", "stress_cpu.py"])
+    return "Stressing CPU in the background!"
+
+@app.route("/", methods=["GET"])
+def get_private_ip():
+    private_ip = socket.gethostbyname(socket.gethostname())
+    return f"Private IP Address: {private_ip}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
